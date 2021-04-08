3334b63f4cc6cc7ed722c8d603d449e263c2ca40
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-04-08 19:27:54 +0900

    モデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :kanji_last_name, :kanji_fist_name, :kana_last_name, :kana_fist_name, :birthday, presence: true
 end
