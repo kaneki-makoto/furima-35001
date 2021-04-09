ac91aef0631a27545b1c39425fc8801fa89241da
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-04-09 19:17:14 +0900

    ログアウト機能を実装と名前とpasswordのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  KANJI_NAME_REGEX =
+
   validates :nickname, :kanji_last_name, :kanji_fist_name, :kana_last_name, :kana_fist_name, :birthday, presence: true
-  has_many :items
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :kanji_last_name
+    validates :kanji_fist_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :kana_last_name
+    validates :kana_fist_name
+  end
+
+    has_many :items
 end
