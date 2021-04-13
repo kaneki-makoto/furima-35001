e590713e14fa7578925861bf12f9be5a57ef9a11
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-04-13 10:09:20 +0900

    ietesテーブルにアソシエーション設定と保存できるよう実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,6 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:product, :explanation, :category_id, :status_id, :burden_id, :area_id, :day_id, :price).merge(user_id: current_user.id )
+    params.require(:item).permit(:product, :explanation, :category_id, :status_id, :burden_id, :area_id, :day_id, :price, :image).merge(user_id: current_user.id )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :product, :explanation, :category_id, :status_id, :burden_id, :area_id, :day_id, :price, presence:true
 end
