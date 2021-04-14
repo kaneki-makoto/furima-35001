c00769303ad58d302f56aceffe4068f27bbb7388
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-04-14 19:33:32 +0900

    バリデーションをwith_optionsでまとめた

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,9 @@ class Item < ApplicationRecord
   validates :product, :image, :explanation, :category_id, :status_id, :burden_id, :area_id, :day_id, :price, presence: true
   validates :product, length: { maximum: 40 }
   validates :explanation, length: { maximum: 1000 }
-  validates :category_id, numericality: { other_than: 1 }
-  validates :status_id, numericality: { other_than: 1 }
-  validates :burden_id, numericality: { other_than: 1 }
-  validates :area_id, numericality: { other_than: 1 }
-  validates :day_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id, :status_id,  :burden_id, :area_id,  :day_id
+  end
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   with_options presence: true, format: { with: /\A[0-9]+\z/ } do
