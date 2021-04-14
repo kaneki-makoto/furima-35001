5fe76c649a11d5de56ca9cc699886c1fe754ec34
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-04-14 15:11:09 +0900

    商品出品の保存機能を実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,21 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: :new
+
   def index
     @items = Item.includes(:user), Item.order("created_at DESC")
   end
 
   def new
-    if user_signed_in?
-      render :new
       @item = Item.new
-    else
-      redirect_to new_user_session_path
-    end
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
