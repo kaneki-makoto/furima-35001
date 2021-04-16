cf45bdf51a87954528c673b08ee071abec27d5a3
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-04-16 12:19:42 +0900

    エラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -32,6 +32,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to action: :index
+    end
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
