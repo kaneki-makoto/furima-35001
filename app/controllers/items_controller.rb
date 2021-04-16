2c13abe0bd5cb598260a26c24276eecf0c4adc65
kaneki-makoto <kanakimakoto@kanagimakotonoMacBook-Pro.local>
2021-04-16 12:45:06 +0900

    インスタンス変数定義をまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, except: [:show, :edit, :update] 
+
   def index
     @items = Item.order('created_at DESC')
   end
@@ -18,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     unless current_user == @item.user
       redirect_to action: :index
     end
@@ -45,5 +44,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:product, :explanation, :category_id, :status_id, :burden_id, :area_id, :day_id, :price,
                                  :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item_Item.find(params[:id])
   end
 end
